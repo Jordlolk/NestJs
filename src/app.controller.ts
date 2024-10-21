5dee86eac3b51daa5cfeae0c187ebff3f94757e1
Jord allef <jordeallefsilvadecarvalho@gmail.com>
2024-10-21 17:19:02 -0300

    feat: new decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,49 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch, Put, Delete, Req, Query, Ip, Headers} from '@nestjs/common';
 import { AppService } from './app.service';
 export let controllerpath = 'controller';
+
 @Controller(controllerpath)
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  //AppController - Área responsável por ajustar os dados e prepar eles para o usuário ver.
   public dados: any;
   @Post() // Aplicação para o servidor
   pushNewData(@Body() { dogs }: any) {
     this.dados = dogs;
     return { message: 'Dados recebidos com sucesso!' };
   }
+  @Get()
+  findURL(@Query('page') quer : any){
+    console.log(JSON.stringify(quer))
+  }
+  @Get('data/dogs')
+  findAll(){
+    return this.dados ? this.appService.findAllDogs(this.dados) : 'API ERROR'
+  }
+
+  @Get('data/:id') // Servidor para aplicação
+  getDadosStatus(@Param('id') id : number) {
+    return this.dados ? this.appService.findOneDog(this.dados,id) : 'API ERROR';
+  }
+
+  @Delete('delete/:delete')
+    takeOutDog(@Param('delete') id : string ){
+      return this.appService.remove_dog(this.dados, parseInt(id))
+  }
+
+  @Get('data/dogs/:random*dog')
+  takeRndomDog(@Param('random*dog') rnd : string) {
+    return this.appService.getRandomDog(this.dados)
+  }
+  //AppController - Área responsável por ajustar os dados e prepar eles para o usuário ver.
   //Os decorators também podem criar rotas automaticamente .
   //Entre os () é necessário por o nome de uma outra rota, porque o NestJs NÃO roda as funções se estiverem na mesma rota/provider.
-  @Get('status') // Servidor para aplicação
-  getDadosStatus() {
-    return this.appService.findOneDog(this.dados, 2);
-  }
-  //Get() é a rota padrão, como : localhost:3000,
+  //Get() é a rota padrão, como o parentêse está vazio a rota será: localhost:3000,
   //qualquer outra alteração é localhost:3000/nomeDaRota @Get(nomeDaRota).
+  //@Post - usado para pegar dados do servidor: alterar ou salvar.
+  //@Get - usado para pegar dados do servidor: deve apenas buscar os dados, podendo apenas mostrar na tela.
+  //@Put - usado para atualizar dados que estão presentes no server (Todos os dados)
+  //@Patch - usado para atualizar um dado específico do server.
+  //Patch e Put são funções que vão diretamente ao servidor, elas não passam a ser vistas na PG.
+  //
+  //@Param(nomeDaRota) - usado para pegar um dado específico da URL, pode ser chamado nos parâmetros de uma função.
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { throws } from 'assert';
+import { error } from 'console';
 
 @Injectable()
 export class AppService {
-  //AppService - Área onde é feita a regra de negócio, quais dados podem ou não serem vistos pelo usuário.
   findAllDogs(dogs: object[]) {
     return dogs.map((dog : {name : string}) => dog.name);
   }
+
   findOneDog(body: Array<{id : number, name : string}>, id: number) {
     if(!Array.isArray(body) || body.length === 0){
       return {message: '[ERROR]: The API returns nothing!'}
     }
    let yourDog : {name : string} =  body.find((dogs : {id:number, name : string}) => dogs.id === id)
-    return yourDog.name? yourDog.name : 'Your dog is not on the list'
+    return yourDog.name? `Your Dog is ${yourDog.name}`: 'Your dog is not on the list'
   }
-  /* getCreate(): number {
-    let randomNumber = Math.floor(Math.random() * (20 - 0 + 1) + 0);
-    return randomNumber;
-  } */
+
+  remove_dog(dogs : object[], id : number){
+    let index = Array.isArray(dogs) ? dogs.findIndex((dog : {id : Number})=> dog.id === id) : null;
+    if(index === -1){
+      return {message: 'The dog not exist or it got already deleted from the list.', dogsList : dogs}
+    }
+    if(!index){
+      throw new Error('API ERROR')
+    }
+    let takedOut = dogs[index]
+    dogs.splice(index,1)
+    return {message: "Dog removed", Dog : takedOut, remainsDogs : dogs};
+  }
+
+  getRandomDog(dogs : object[]): object {
+    let arrOfIDS = dogs.map((dog : {id:number}) => {return dog.id})
+    let maxValue = Math.max(...arrOfIDS), minValue = Math.min(...arrOfIDS)
+    let randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+    return dogs[randomNumber];
+  }
 }
