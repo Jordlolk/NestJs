c3abc6099ab45be04dae308ef55b73274a5000a0
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2024-10-21 12:08:01 -0300

    testing new concepts and functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, Post} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller('api')
+export let controllerpath = 'controller';
+@Controller(controllerpath)
 export class AppController {
   constructor(private readonly appService: AppService) {}
   //AppController - Área responsável por ajustar os dados e prepar eles para o usuário ver.
-
-  @Get('dados') // @Get() responsável por criar as rotas para a aplicação.
+  public dados: any;
+  @Post() // Aplicação para o servidor
+  pushNewData(@Body() { dogs }: any) {
+    this.dados = dogs;
+    return { message: 'Dados recebidos com sucesso!' };
+  }
+  //Os decorators também podem criar rotas automaticamente .
   //Entre os () é necessário por o nome de uma outra rota, porque o NestJs NÃO roda as funções se estiverem na mesma rota/provider.
-  getDadosStatus(){
-    return {message: 'Dados retornados com sucesso!'}
+  @Get('status') // Servidor para aplicação
+  getDadosStatus() {
+    return this.appService.findOneDog(this.dados, 2);
   }
-  //Get() é a rota padrão, como : localhost:3000, qualquer outra alteração é localhost:3000/nomeDaRota.
-  @Post('dados')
-  postDados(@Body() body : any): any {
-    return { message: 'Dados recebidos: ', data : body};
-  }
+  //Get() é a rota padrão, como : localhost:3000,
+  //qualquer outra alteração é localhost:3000/nomeDaRota @Get(nomeDaRota).
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-//AppService - Área onde é feita a regra de negócio, quais dados podem ou não serem vistos pelo usuário.
-  getHello(): string {
-    return 'Hello World!';
+  //AppService - Área onde é feita a regra de negócio, quais dados podem ou não serem vistos pelo usuário.
+  findAllDogs(dogs: object[]) {
+    return dogs.map((dog : {name : string}) => dog.name);
   }
-  getCreate(): string{
-    return `
-      Version - 0.0.0
-      O que eu aprendi com NestJs, é possivél puxar modules, controllers e providers.
-      controllers pegam requisições http e providers criam rotas, essas rotas diferente do express
-      podem ser criadas apenas usando um Decorator, logo acima da função desejada. 
-    `
+  findOneDog(body: Array<{id : number, name : string}>, id: number) {
+    if(!Array.isArray(body) || body.length === 0){
+      return {message: '[ERROR]: The API returns nothing!'}
+    }
+   let yourDog : {name : string} =  body.find((dogs : {id:number, name : string}) => dogs.id === id)
+    return yourDog.name? yourDog.name : 'Your dog is not on the list'
   }
+  /* getCreate(): number {
+    let randomNumber = Math.floor(Math.random() * (20 - 0 + 1) + 0);
+    return randomNumber;
+  } */
 }
